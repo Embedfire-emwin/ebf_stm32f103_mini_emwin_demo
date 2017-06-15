b80ff0f75003febb3cbefca6700f1574dd6eae46
flyleaf91 <flyleaf91@163.com>
2017-06-15 17:10:46 +0800

    添加emwin例程

---
+++ 17. ucos_emWin_显示图片/uCOSIII/os_cfg/app_cfg.h
@@ -0,0 +1,107 @@
+/*
+*********************************************************************************************************
+*                                              EXAMPLE CODE
+*
+*                           (c) Copyright 2009-2013; Micrium, Inc.; Weston, FL
+*
+*               All rights reserved.  Protected by international copyright laws.
+*               Knowledge of the source code may NOT be used to develop a similar product.
+*               Please help us continue to provide the Embedded community with the finest
+*               software available.  Your honesty is greatly appreciated.
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                      APPLICATION CONFIGURATION
+*
+*                                     ST Microelectronics STM32
+*                                              on the
+*
+*                                     Micrium uC-Eval-STM32F107
+*                                         Evaluation Board
+*
+* Filename      : app_cfg.h
+* Version       : V1.00
+* Programmer(s) : JJL
+*                 EHS
+*                 DC
+*********************************************************************************************************
+*/
+
+#ifndef  __APP_CFG_H__
+#define  __APP_CFG_H__
+
+
+/*
+*********************************************************************************************************
+*                                       MODULE ENABLE / DISABLE
+*********************************************************************************************************
+*/
+
+#define  APP_CFG_SERIAL_EN                          DEF_DISABLED          // Modified by fire £¨Ô­ÊÇ DEF_ENABLED£©
+
+/*
+*********************************************************************************************************
+*                                            TASK PRIORITIES
+*********************************************************************************************************
+*/
+#define  APP_TASK_KEY_SCAN_PRIO                       2u
+#define  APP_TASK_GUI_BASE_PRIO                       3u
+#define  APP_TASK_START_PRIO                        	4u
+
+
+/*
+*********************************************************************************************************
+*                                            TASK STACK SIZES
+*                             Size of the task stacks (# of OS_STK entries)
+*********************************************************************************************************
+*/
+
+#define  APP_TASK_GUI_BASE_STK_SIZE                 1024u
+#define  APP_TASK_START_STK_SIZE                    128u
+#define  APP_TASK_KEY_SCAN_STK_SIZE                 1024u
+
+
+/*
+*********************************************************************************************************
+*                                    BSP CONFIGURATION: RS-232
+*********************************************************************************************************
+*/
+
+#define  BSP_CFG_SER_COMM_SEL             			BSP_SER_COMM_UART_02
+#define  BSP_CFG_TS_TMR_SEL                         2
+
+
+/*
+*********************************************************************************************************
+*                                     TRACE / DEBUG CONFIGURATION
+*********************************************************************************************************
+*/
+
+#ifndef  TRACE_LEVEL_OFF
+#define  TRACE_LEVEL_OFF                0
+#endif
+
+#ifndef  TRACE_LEVEL_INFO
+#define  TRACE_LEVEL_INFO               1
+#endif
+
+#ifndef  TRACE_LEVEL_DBG
+#define  TRACE_LEVEL_DBG                2
+#endif
+
+#define  APP_CFG_TRACE_LEVEL             TRACE_LEVEL_DBG
+#define  APP_CFG_TRACE                   printf
+
+#define  BSP_CFG_TRACE_LEVEL             TRACE_LEVEL_OFF
+#define  BSP_CFG_TRACE                   printf
+
+#define  APP_TRACE_INFO(x)               ((APP_CFG_TRACE_LEVEL >= TRACE_LEVEL_INFO)  ? (void)(APP_CFG_TRACE x) : (void)0)
+#define  APP_TRACE_DBG(x)                ((APP_CFG_TRACE_LEVEL >= TRACE_LEVEL_DBG)   ? (void)(APP_CFG_TRACE x) : (void)0)
+
+#define  BSP_TRACE_INFO(x)               ((BSP_CFG_TRACE_LEVEL  >= TRACE_LEVEL_INFO) ? (void)(BSP_CFG_TRACE x) : (void)0)
+#define  BSP_TRACE_DBG(x)                ((BSP_CFG_TRACE_LEVEL  >= TRACE_LEVEL_DBG)  ? (void)(BSP_CFG_TRACE x) : (void)0)
+
+
+#endif
