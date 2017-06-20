7f06d5940d99eb9e2ca560f82d4bae3e073ecabc
flyleaf91 <flyleaf91@163.com>
2017-06-20 17:42:30 +0800

    增加桌面，LED，天气，ADC等例程

---
+++ 18. ucos_emWin_创建桌面/User/APP/main.c
@@ -0,0 +1,279 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2015-xx-xx
+  * @brief   ucos_emWin_FatFs移植_外部编码表
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:秉火  STM32 F103 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************************
+  */
+
+
+/*
+*********************************************************************************************************
+*                                             包含文件
+*********************************************************************************************************
+*/
+
+#include <includes.h>
+
+
+/*
+*********************************************************************************************************
+*                                               宏定义
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                             任务控制块TCB
+*********************************************************************************************************
+*/
+
+static  OS_TCB   AppTaskStartTCB;
+static  OS_TCB   AppTaskKeyScanTCB;
+static  OS_TCB   AppTaskDesktopTCB;
+
+
+/*
+*********************************************************************************************************
+*                                              栈空间STACKS
+*********************************************************************************************************
+*/
+__align(8) static  CPU_STK  AppTaskStartStk[APP_TASK_START_STK_SIZE];
+__align(8) static  CPU_STK  AppTaskKeyScanStk[APP_TASK_KEY_SCAN_STK_SIZE];
+__align(8) static  CPU_STK  AppTaskDesktopStk[APP_TASK_DESKTOP_STK_SIZE];
+
+
+/*
+*********************************************************************************************************
+*                                             函数声明
+*********************************************************************************************************
+*/
+
+static  void  AppTaskStart  (void *p_arg);
+static  void  BSPTaskCreate (void);
+static  void  AppTaskCreate(void);
+
+
+/*
+*********************************************************************************************************
+*                                            
+*********************************************************************************************************
+*/
+
+
+/**
+  * @brief  主函数
+  * @param  无  
+  * @retval 无
+  */
+int  main (void)
+{
+    OS_ERR  os_err;
+
+    OSInit(&os_err);                                               		/* 初始化 uC/OS-III.                                      */
+
+		//创建启动任务
+    OSTaskCreate((OS_TCB     *)&AppTaskStartTCB,                // 任务TCB                               
+                 (CPU_CHAR   *)"App Task Start", 								// 任务名称                             
+                 (OS_TASK_PTR ) AppTaskStart,									  // 任务函数指针                                
+                 (void       *) 0,																	// 可选输入数据
+                 (OS_PRIO     ) APP_TASK_START_PRIO,							// 优先级
+                 (CPU_STK    *)&AppTaskStartStk[0],							// 任务栈基地址
+                 (CPU_STK_SIZE) APP_TASK_START_STK_SIZE / 10,		// 栈“水印”限制
+                 (CPU_STK_SIZE) APP_TASK_START_STK_SIZE,        //栈大小
+                 (OS_MSG_QTY  ) 5u,
+                 (OS_TICK     ) 0u,
+                 (void       *) 0,
+                 (OS_OPT      )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),//可选配置
+                 (OS_ERR     *)&os_err);															//错误代码
+
+		 //检查错误
+		if(os_err!=OS_ERR_NONE)  bsp_result|=BSP_OS;
+
+		//开始执行任务，从此处开始由ucos系统调度
+    OSStart(&os_err);  
+
+	  //检查错误，正常的程序已由ucos调度，不应运行到此处
+		if(os_err!=OS_ERR_NONE)  bsp_result|=BSP_OS;
+		
+		BSP_TRACE_INFO(("OS err:0x%x",bsp_result));
+                                         
+		
+		
+}
+
+
+/**
+  * @brief  启动任务函数，主要完成ucos、BSP、GUI以及其它任务的初始化
+  * @param  p_arg: OSTaskCreate创建时传入的数据指针
+  * @retval 无
+  */
+static  void  AppTaskStart (void *p_arg)
+{
+    OS_ERR      os_err;
+
+   (void)p_arg;
+  
+		BSP_Init();  //初始化BSP  	
+		
+		CPU_Init();	//初始化CPU
+
+		BSP_Tick_Init();//初始化systick
+
+    Mem_Init(); 	//初始化存储管理器                                     
+
+#if OS_CFG_STAT_TASK_EN > 0u
+    OSStatTaskCPUUsageInit(&os_err); //计算无任务时CPU使用率                            
+#endif
+
+#ifdef CPU_CFG_INT_DIS_MEAS_EN
+    CPU_IntDisMeasMaxCurReset();
+#endif
+
+
+	APP_TRACE_DBG(("\r\n正在创建驱动、应用任务..."));
+
+	//创建驱动任务
+	BSPTaskCreate();
+	//创建应用任务
+	AppTaskCreate();  
+
+	//检查硬件错误
+	Hardware_Error_Check();
+	
+ //任务死循环
+	while (DEF_TRUE) 
+	{                                        
+			LED2_TOGGLE ;
+			APP_TRACE_DBG(("cpu使用率:%.2f%%\n",((float)OSStatTaskCPUUsage/100)));
+
+  		//延时，所有任务函数的死循环内都应有至少1ms延时
+		  //特别是高优先级的任务，若无延时，其它低优先级任务可能会无机会执行
+			OSTimeDly(1000u, 
+									OS_OPT_TIME_DLY,
+									&os_err);
+	}	
+		
+}
+
+/**
+  * @brief  按键任务函数
+  * @param  无
+  * @retval 无
+  */
+void  AppTaskKeyScan  (void )
+{
+
+	OS_ERR  os_err;
+	
+		  /* 创建事件标志组 flag_grp */
+  OSFlagCreate ((OS_FLAG_GRP  *)&key_flag_grp,        //指向事件标志组的指针
+                (CPU_CHAR     *)"KEY Flag",  //事件标志组的名字
+                (OS_FLAGS      )0,                //事件标志组的初始值
+                (OS_ERR       *)&os_err);					  //返回错误类型
+
+
+	while(1)
+	{
+			GUI_TOUCH_Exec();
+		
+			if( Key_Scan(KEY1_GPIO_PORT,KEY1_GPIO_PIN) == KEY_ON  )
+			{
+						OSFlagPost ((OS_FLAG_GRP  *)&key_flag_grp,                             //将标志组的BIT0清0
+													(OS_FLAGS      )EVEN_KEY1_DOWN,
+													(OS_OPT        )OS_OPT_POST_FLAG_SET,
+													(OS_ERR       *)&os_err);
+			}
+			else
+			{
+			
+			}
+			
+			if( Key_Scan(KEY2_GPIO_PORT,KEY2_GPIO_PIN) == KEY_ON  )
+			{
+						OSFlagPost ((OS_FLAG_GRP  *)&key_flag_grp,                             //将标志组的BIT0清0
+													(OS_FLAGS      )EVEN_KEY2_DOWN,
+													(OS_OPT        )OS_OPT_POST_FLAG_SET,
+													(OS_ERR       *)&os_err);
+			}
+			else
+			{
+			
+			}
+			
+			//延时，所有任务函数的死循环内都应有至少1ms延时
+		  //特别是高优先级的任务，若无延时，其它低优先级任务可能会无机会执行
+			OSTimeDly(10u, 
+									OS_OPT_TIME_DLY,
+									&os_err);
+
+			
+	}
+}
+	
+	
+/**
+  * @brief  创建驱动任务，如按键，定时扫描等
+  * @param  无
+  * @retval 无
+  */
+static  void  BSPTaskCreate (void)
+{
+	OS_ERR  os_err;
+
+		//创建扫描任务
+	OSTaskCreate((OS_TCB     *)&AppTaskKeyScanTCB,             // 任务TCB                               
+							 (CPU_CHAR   *)"Key Scan", 									// 任务名称                             
+							 (OS_TASK_PTR ) AppTaskKeyScan,									  // 任务函数指针                                
+							 (void       *) 0,																	// 可选输入数据
+							 (OS_PRIO     ) APP_TASK_KEY_SCAN_PRIO,					// 优先级
+							 (CPU_STK    *)&AppTaskKeyScanStk[0],							// 任务栈基地址
+							 (CPU_STK_SIZE) APP_TASK_KEY_SCAN_STK_SIZE / 10,				// 栈“水印”限制
+							 (CPU_STK_SIZE) APP_TASK_KEY_SCAN_STK_SIZE,        		//栈大小
+							 (OS_MSG_QTY  ) 0u,
+							 (OS_TICK     ) 0u,
+							 (void       *) 0,
+							 (OS_OPT      )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),//可选配置
+							 (OS_ERR     *)&os_err);															//错误代码
+
+
+}
+
+/**
+  * @brief  创建应用任务的函数，它会被启动任务调用以创建应用
+  * @param  无
+  * @retval 无
+  */
+static  void  AppTaskCreate (void)
+{
+	OS_ERR  os_err;
+
+		//创建应用任务,测试显示图片
+	OSTaskCreate((OS_TCB     *)&AppTaskDesktopTCB,             // 任务TCB                               
+							 (CPU_CHAR   *)"GUI Task", 									// 任务名称                             
+							 (OS_TASK_PTR ) AppTaskDesktop,									  // 任务函数指针                                
+							 (void       *) 0,																	// 可选输入数据
+							 (OS_PRIO     ) APP_TASK_DESKTOP_PRIO,					// 优先级
+							 (CPU_STK    *)&AppTaskDesktopStk[0],							// 任务栈基地址
+							 (CPU_STK_SIZE) APP_TASK_DESKTOP_STK_SIZE / 10,				// 栈“水印”限制
+							 (CPU_STK_SIZE) APP_TASK_DESKTOP_STK_SIZE,        		//栈大小
+							 (OS_MSG_QTY  ) 0u,
+							 (OS_TICK     ) 0u,
+							 (void       *) 0,
+							 (OS_OPT      )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),//可选配置
+							 (OS_ERR     *)&os_err);															//错误代码
+
+
+}
+
+/*********************************************END OF FILE**********************/
+
---
+++ 22. ucos_emWin_创建ADC应用/uCOSIII/os_cfg/includes.h
@@ -0,0 +1,131 @@
+/*
+*********************************************************************************************************
+*                                              EXAMPLE CODE
+*
+*                          (c) Copyright 2003-2013; Micrium, Inc.; Weston, FL
+*
+*               All rights reserved.  Protected by international copyright laws.
+*               Knowledge of the source code may NOT be used to develop a similar product.
+*               Please help us continue to provide the Embedded community with the finest
+*               software available.  Your honesty is greatly appreciated.
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*
+*                                           MASTER INCLUDES
+*
+*                                     ST Microelectronics STM32
+*                                              on the
+*
+*                                     Micrium uC-Eval-STM32F107
+*                                         Evaluation Board
+*
+* Filename      : includes.h
+* Version       : V1.00
+* Programmer(s) : EHS
+*                 DC
+*********************************************************************************************************
+*/
+
+#ifndef  INCLUDES_PRESENT
+#define  INCLUDES_PRESENT
+
+//使能hardfault时直接软件复位
+//#ifndef SOFTRESET
+//#define SOFTRESET
+//#endif
+
+//SPI FLASH存储触摸校准数据的位置
+#define CALADD  				(510*4096)
+#define CALADD_FLAG		0x55
+
+extern unsigned char IsCal ;
+
+/*
+*********************************************************************************************************
+*                                         STANDARD LIBRARIES
+*********************************************************************************************************
+*/
+
+#include  <stdarg.h>
+#include  <stdio.h>
+#include  <stdlib.h>
+#include	<string.h>
+#include  <math.h>
+
+
+/*
+*********************************************************************************************************
+*                                              LIBRARIES
+*********************************************************************************************************
+*/
+
+#include  <cpu.h>
+#include  <lib_def.h>
+#include  <lib_ascii.h>
+#include  <lib_math.h>
+#include  <lib_mem.h>
+#include  <lib_str.h>
+
+
+/*
+*********************************************************************************************************
+*                                              APP / BSP
+*********************************************************************************************************
+*/
+#include  <app_cfg.h>
+#include  <bsp.h>
+#include "./usart/bsp_usart.h"
+#include "./led/bsp_led.h"
+#include "./key/bsp_key.h"  
+#include "./lcd/bsp_ili9341_lcd.h"
+#include "./dht11/bsp_dht11.h"
+#include "./ds18b20/bsp_ds18b20.h"
+#include "./adc/bsp_adc.h"
+
+
+#include "ff.h"
+
+#include "GUIFont_Port.h" 
+#include "GUIPicture_Port.h"   
+
+#include "desktop.h"
+
+/*
+*********************************************************************************************************
+*                                               emWin
+*********************************************************************************************************
+*/
+#include "GUI.h"
+#include "DIALOG.h"
+
+
+/*
+*********************************************************************************************************
+*                                                 OS
+*********************************************************************************************************
+*/
+
+#include  <os.h>
+
+
+/*
+*********************************************************************************************************
+*                                                 ST
+*********************************************************************************************************
+*/
+
+#include "stm32f10x.h"          // Modified by fire (原是 #include  <stm32f10x_lib.h>) 
+
+
+/*
+*********************************************************************************************************
+*                                            INCLUDES END
+*********************************************************************************************************
+*/
+
+
+#endif
+
